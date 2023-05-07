99aace8f31c316a19ec40853fe482ed4e5b0da47
HaythmKenway <akileswar2002@protonmail>
2023-05-07 17:14:26 +0530

    Init

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="min-w-[1150px] bg-gray-100 h-full">
+	<div class="flex items-center bg-gray-900 h-[60px] py-2 fixed z-50 min-w-[1150px] w-full">
+<div class="flex">
+  <nuxt-link class="text-white h-[50px] p-2 pt-3 border-[1px] border-gray-900 rounded-sm hover:border-[1px] cursor-pointer">Shopbox</nuxt-link>
+  </div>
+  <div class="flex items-center justify-center">
+	  <div class="text-[13px] text-gray-300 font-extrabold">
+		  <div>Delivery to John
+	  </div></div>
+	  <div class="text-[12px] text-white -mt-1.5 font-extrabold">
+		  <div>Tamilnadu</div></div>
+
+  </div>
+</div></div>
+
+</template>
