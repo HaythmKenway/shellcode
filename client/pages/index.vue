d32a644449286dc0fb2698b1e18d087dccf2e9ed
Akileswar <HaythmKenway>
2023-06-02 09:48:29 +0530

    ADD: catalogue item Modify: Navbar

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,9 +1,13 @@
-
 <template>
-	<Navbar />	
+  <div>
+    <Navbar />
+    <Catalogue title="giggity" body="Lorem ipsum dolor sit samet consectetur adipisicing elit. Nesciunt perferendis molestias, veritati" rating=4 image='https://media.istockphoto.com/id/1411610158/photo/multi-colored-programming-language-source-code-design-example-front-view-composition-on-a.jpg?s=2048x2048&w=is&k=20&c=YCUdVdfVlX0UA9VHZI4hRnl8P6rGGrP8pjbeXhdb-x4='/>
+  </div>
 </template>
+
 <style>
-	*{
-		background-color: rgb(32,33,36);
-	}
+  * {
+    background-color: rgb(32, 33, 36);
+  }
 </style>
+
